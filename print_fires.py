b52cb528c797714b3b7d4542eb7d61d5406bd1f1
Jasmin Chadha <Jasmin.Chadha@colorado.edu>
2025-09-09 09:07:51 -0600

    fixed syntax errors, column numbers, added import of my_utils and correct function call with inputs to print the number of fires in USA

--- print_fires.py
+++ print_fires.py
@@ -1,6 +1,12 @@
-country='United States of America'
-county_column = 1
-fires_column = 4
+# Import my_utils with the get_column function
+import my_utils
+
+# Define inputs from file for function
+country = 'United States of America'
+country_column = 0
+fires_column = 3
 file_name = 'Agrofood_co2_emission.csv'
-fires = get_column()
+
+# Call function and print results
+fires = my_utils.get_column(file_name, country_column, country, fires_column)
 print(fires)
