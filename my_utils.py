50c43a3071467c61990f1e46f4a6ee3873bd6698
Jasmin Chadha <Jasmin.Chadha@colorado.edu>
2025-09-09 08:35:29 -0600

    created a function that splits each line of a file into an array, checks if values match in two columns, and returns a result array
    created a function that splits each line of a file into an array, checks if values match in two columns, and returns a result array

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,24 @@
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    # Open file for reading
+    f = open(file_name, 'r')
+
+    # Initialize array for results
+    resultsarray = []
+
+    # Skip header
+    next(f)
+
+    # Process file line by line
+    for line in f:
+        # Remove newline character and split into arrays by line
+        linearraymat = line.rstrip().split(',')
+        # Check if value in query_column matches the value in query_value
+        if linearraymat[query_column] == query_value:
+            # If values match, add the value from result_column to resultsarray
+            resultsarray.append(linearraymat[result_column])
+
+    # Close the file
+    f.close()
+
+    # Return resultsarray so it can be used outside the function
+    return resultsarray
